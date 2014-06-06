7b5040dd15e391de231d57f758337c9055cf15e9
skfd <ksen.knev@gmail.com>
2014-06-07 00:30:40 +0300

    uploader pulls repos for specific org

---
+++ RepoReUploader.Tests/UploaderTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using RepoReUploader.Uploader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoReUploader.Tests
+{
+    [TestFixture]
+    public class UploaderTests
+    {
+        [Test]
+        public void OrganisationsReposRetrievedLive()
+        {
+            var d = new GithubService();
+
+            var repos = d.GetPublicRepos("octokit");
+
+            Assert.IsNotEmpty(repos);
+        }
+    }
+}
---
+++ RepoReUploader.Uploader/GithubService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Octokit;
+
+namespace RepoReUploader.Uploader
+{
+    public class GithubService
+    {
+        public GithubService()
+        {
+            this.Github = new GitHubClient(new ProductHeaderValue("RepoReUploader"));
+        }
+        public IReadOnlyList<string> GetPublicRepos(string organisationName)
+        {
+            var org = Github.Repository.GetAllForOrg(organisationName).Result;
+
+            var repos = org.Select(x => x.Name).ToList();
+
+            return repos;
+        }
+
+        public GitHubClient Github { get; set; }
+    }
+}
